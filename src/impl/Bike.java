d337f2f68e81020753cbe6bfb2117f41c655f4ce
Bhavik Banchpalliwar <war@amazon.com>
2021-04-17 18:45:38 +0530

    Enhancement 2 for bike. For issue #3

--- src/impl/Bike.java
+++ src/impl/Bike.java
@@ -5,16 +5,16 @@ import assignmentG10.vehicle.Vehicle;
 public class Bike implements Vehicle {
     @Override
     public void shiftGear() {
-        System.out.println("Upgraded Shifting gear of Bike");
+        System.out.println("Upgraded ver 2 of Shifting gear of Bike");
     }
 
     @Override
     public void applyBreak() {
-        System.out.println("Upgraded Applying break for Bike");
+        System.out.println("Upgraded ver 2 of Applying break for Bike");
     }
 
     @Override
     public void honkHorn() {
-        System.out.println("Upgraded honk horn gear for Bike");
+        System.out.println("Upgraded ver 2 of honk horn gear for Bike");
     }
 }
